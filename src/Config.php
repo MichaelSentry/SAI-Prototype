2514200ce3a087254167d87e2fa05edfdeddfe29
MichaelSentry <gitsentry@ninjasentry.com>
2016-02-13 14:13:57 +1100

    First commit for config class

---
+++ src/Config.php
@@ -0,0 +1,22 @@
+<?php
+namespace NinjaSentry\Sai;
+
+/**
+ * SAI Prototype
+ * ------------------------------------
+ * Config Class
+ *
+ * ninjasentry.com 2016
+ */
+
+/**
+ * The motive behind criticism often determines its validity.
+ * Those who care, criticize where necessary.
+ * Those who envy, criticize the moment they think that they have found a weak spot.
+ * ~ Criss Jami
+ */
+
+class Config
+{
+    public function __construct(){}
+}
